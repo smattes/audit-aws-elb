2d3ffb4e0c69373718e0e5dd34a90facbc819e32
Joe Hicks <joe@cloudcoreo.com>
2016-10-06 10:38:05 -0700

    Set payload type to json.

--- services/config.rb
+++ services/config.rb
@@ -29,7 +29,7 @@ coreo_uni_util_notify "advise-elb" do
   "number_of_violations":"STACK::coreo_aws_advisor_elb.advise-elb.number_violations",
   "number_violations_ignored":"STACK::coreo_aws_advisor_elb.advise-elb.number_ignored_violations",
   "violations": STACK::coreo_aws_advisor_elb.advise-elb.report }'
-  payload_type "${AUDIT_AWS_ELB_PAYLOAD_TYPE}"
+  payload_type "json"
   endpoint ({
       :to => '${AUDIT_AWS_ELB_ALERT_RECIPIENT}', :subject => 'CloudCoreo elb advisor alerts on INSTANCE::stack_name :: INSTANCE::name'
   })
