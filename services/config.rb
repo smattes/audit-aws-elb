d073f43d38d8bb1be8a214eb6021a8a3c686e8d2
George Moberly <georgemoberly@Georges-MacBook-Air-2.local>
2016-10-21 09:25:49 -0700

    extra rules for testing

--- services/config.rb
+++ services/config.rb
@@ -48,22 +48,22 @@ coreo_aws_advisor_elb "advise-elb" do
 end
 
 ## This is the normal full notification of the primary recipient.
-# coreo_uni_util_notify "advise-elb" do
-#   action :notify
-#   type 'email'
-#   allow_empty ${AUDIT_AWS_ELB_ALLOW_EMPTY}
-#   send_on "${AUDIT_AWS_ELB_SEND_ON}"
-#   payload '{"stack name":"INSTANCE::stack_name",
-#   "instance name":"INSTANCE::name",
-#   "number_of_checks":"STACK::coreo_aws_advisor_elb.advise-elb.number_checks",
-#   "number_of_violations":"STACK::coreo_aws_advisor_elb.advise-elb.number_violations",
-#   "number_violations_ignored":"STACK::coreo_aws_advisor_elb.advise-elb.number_ignored_violations",
-#   "violations": STACK::coreo_aws_advisor_elb.advise-elb.report }'
-#   payload_type "json"
-#   endpoint ({
-#       :to => '${AUDIT_AWS_ELB_ALERT_RECIPIENT}', :subject => 'CloudCoreo elb advisor alerts on INSTANCE::stack_name :: INSTANCE::name'
-#   })
-# end
+coreo_uni_util_notify "advise-elb" do
+  action :notify
+  type 'email'
+  allow_empty ${AUDIT_AWS_ELB_ALLOW_EMPTY}
+  send_on "${AUDIT_AWS_ELB_SEND_ON}"
+  payload '{"stack name":"INSTANCE::stack_name",
+  "instance name":"INSTANCE::name",
+  "number_of_checks":"STACK::coreo_aws_advisor_elb.advise-elb.number_checks",
+  "number_of_violations":"STACK::coreo_aws_advisor_elb.advise-elb.number_violations",
+  "number_violations_ignored":"STACK::coreo_aws_advisor_elb.advise-elb.number_ignored_violations",
+  "violations": STACK::coreo_aws_advisor_elb.advise-elb.report }'
+  payload_type "json"
+  endpoint ({
+      :to => '${AUDIT_AWS_ELB_ALERT_RECIPIENT}', :subject => 'CloudCoreo elb advisor alerts on INSTANCE::stack_name :: INSTANCE::name'
+  })
+end
 
 ## This is part of tag parsing code.
 coreo_uni_util_jsrunner "tags-to-notifiers-array" do
@@ -210,7 +210,7 @@ for (var entry=0; entry < json_input.length; entry++) {
     console.log('got an email to rollup');
     //nViolations = json_input[entry]['payload']['violations'].length;
     //rollup.push({'recipient': json_input[entry]['endpoint']['to'], 'nViolations': nViolations});
-    rollup_string = rollup_string + "recipient: " + json_input[entry]['endpoint']['to'] + " - " + "nViolations: " + nViolations + "\\n";
+    rollup_string = rollup_string + "recipient: " + json_input[entry]['endpoint']['to'] + " - " + "nViolations: " + "\\n";
   }
 }
 callback(rollup_string);
