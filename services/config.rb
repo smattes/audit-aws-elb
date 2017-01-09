75e480122d6b903f70b30e58451a0a15b02aecd8
colemai <colemai@tcd.ie>
2017-01-09 11:34:30 -0800

    CON-148 added id maps

--- services/config.rb
+++ services/config.rb
@@ -12,6 +12,7 @@ coreo_aws_advisor_alert "elb-inventory" do
   audit_objects ["load_balancer_descriptions.load_balancer_name"]
   operators ["=~"]
   alert_when [//]
+  id_map "object.load_balancer_descriptions.load_balancer_name"
 end
 
 coreo_aws_advisor_alert "elb-old-ssl-policy" do
@@ -30,6 +31,7 @@ coreo_aws_advisor_alert "elb-old-ssl-policy" do
   formulas       ["", "jmespath.[].policy_attribute_descriptions[?attribute_name == 'Reference-Security-Policy'].attribute_value"]
   operators      ["", "!~"]
   alert_when     ["", /\[\"?(?:ELBSecurityPolicy-2016-08)?\"?\]/]
+  id_map "object.load_balancer_descriptions.load_balancer_name"
 end
 
 coreo_aws_advisor_alert "elb-current-ssl-policy" do
@@ -49,6 +51,7 @@ coreo_aws_advisor_alert "elb-current-ssl-policy" do
   formulas       ["", "jmespath.[].policy_attribute_descriptions[?attribute_name == 'Reference-Security-Policy'].attribute_value"]
   operators      ["", "=~"]
   alert_when     ["", /\[\"?(?:ELBSecurityPolicy-2016-08)?\"?\]/]
+  id_map "object.load_balancer_descriptions.load_balancer_name"
 end
 
 coreo_aws_advisor_elb "advise-elb" do
