7053d183fa6d5860672462d96f3368695a0e5301
Michael Pautov <123prove123@gmail.com>
2017-02-01 16:10:44 +0600

    update jsrunner

--- services/config.rb
+++ services/config.rb
@@ -186,7 +186,7 @@ coreo_uni_util_jsrunner "elb-tags-to-notifiers-array" do
   packages([
                {
                    :name => "cloudcoreo-jsrunner-commons",
-                   :version => "1.7.0"
+                   :version => "1.7.3"
                }       ])
   json_input '{ "composite name":"PLAN::stack_name",
                 "plan name":"PLAN::name",
