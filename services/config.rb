6902e059d00744cbbc66cf67b8e48dde302294da
Michael Pautov <michael.pautov@gmail.com>
2017-03-09 13:48:43 +0600

    Changed * to latest

--- services/config.rb
+++ services/config.rb
@@ -87,7 +87,7 @@ coreo_uni_util_jsrunner "elb-tags-to-notifiers-array" do
   packages([
                {
                    :name => "cloudcoreo-jsrunner-commons",
-                   :version => "*"
+                   :version => "latest"
                },
                {
                    :name => "js-yaml",
