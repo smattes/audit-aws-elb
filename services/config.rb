e975d07e1b42a50424fb6ee2c1bb09fda6d2a2bf
Michael Pautov <michael.pautov@gmail.com>
2017-04-07 11:12:29 +0600

    Changed planwide.number_violations to GLOBAL.number-violations

--- services/config.rb
+++ services/config.rb
@@ -61,7 +61,7 @@ coreo_uni_util_variables "elb-planwide" do
                 {'COMPOSITE::coreo_uni_util_variables.elb-planwide.composite_name' => 'PLAN::stack_name'},
                 {'COMPOSITE::coreo_uni_util_variables.elb-planwide.plan_name' => 'PLAN::name'},
                 {'COMPOSITE::coreo_uni_util_variables.elb-planwide.results' => 'unset'},
-                {'COMPOSITE::coreo_uni_util_variables.elb-planwide.number_violations' => '0'}
+                {'GLOBAL::number_violations' => '0'}
             ])
 end
 
@@ -75,7 +75,7 @@ coreo_uni_util_variables "elb-update-planwide-1" do
   action :set
   variables([
                 {'COMPOSITE::coreo_uni_util_variables.elb-planwide.results' => 'COMPOSITE::coreo_aws_rule_runner_elb.advise-elb.report'},
-                {'COMPOSITE::coreo_uni_util_variables.elb-planwide.number_violations' => 'COMPOSITE::coreo_aws_rule_runner_elb.advise-elb.number_violations'},
+                {'GLOBAL::number_violations' => 'COMPOSITE::coreo_aws_rule_runner_elb.advise-elb.number_violations'},
 
             ])
 end
