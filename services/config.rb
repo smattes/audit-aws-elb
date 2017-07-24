dc6ee09df55bdde706e5b59e86ac260b02d0223e
omurbek.jk@gmail.com <omurbek.jk@gmail.com>
2017-07-24 16:09:44 +0600

    update jsrunner to beta57

--- services/config.rb
+++ services/config.rb
@@ -107,7 +107,7 @@ coreo_uni_util_jsrunner "elb-tags-to-notifiers-array" do
   packages([
                {
                    :name => "cloudcoreo-jsrunner-commons",
-                   :version => "1.10.7-beta54"
+                   :version => "1.10.7-beta57"
                },
                {
                    :name => "js-yaml",
