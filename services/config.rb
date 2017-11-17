6731cb9a0d3e967ed47ea376dd511ded7311b18f
omurbek <omurbek.jk@gmail.com>
2017-11-17 17:11:30 +0600

    PLA-5161-prepare-our-content-hub-for-suppressions-v2

--- services/config.rb
+++ services/config.rb
@@ -106,7 +106,7 @@ coreo_uni_util_jsrunner "elb-tags-to-notifiers-array" do
   packages([
                {
                    :name => "cloudcoreo-jsrunner-commons",
-                   :version => "1.10.7-beta65"
+                   :version => "1.10.7-beta77"
                },
                {
                    :name => "js-yaml",
@@ -134,28 +134,10 @@ const htmlReportSubject = "${HTML_REPORT_SUBJECT}";
 const alertListArray = ${AUDIT_AWS_ELB_ALERT_LIST};
 const ruleInputs = {};
 
-let userSuppression;
 let userSchemes;
 
 const fs = require('fs');
 const yaml = require('js-yaml');
-
-function setSuppression() {
-  try {
-      userSuppression = yaml.safeLoad(fs.readFileSync('./suppression.yaml', 'utf8'));
-  } catch (e) {
-    if (e.name==="YAMLException") {
-      throw new Error("Syntax error in suppression.yaml file. "+ e.message);
-    }
-    else{
-      console.log(e.name);
-      console.log(e.message);
-      userSuppression=[];
-    }
-  }
-
-  coreoExport('suppression', JSON.stringify(userSuppression));
-}
 
 function setTable() {
   try {
@@ -173,11 +155,10 @@ function setTable() {
 
   coreoExport('table', JSON.stringify(userSchemes));
 }
-setSuppression();
 setTable();
 
 const argForConfig = {
-    NO_OWNER_EMAIL, cloudObjects, userSuppression, OWNER_TAG,
+    NO_OWNER_EMAIL, cloudObjects, OWNER_TAG,
     userSchemes, alertListArray, ruleInputs, ALLOW_EMPTY,
     SEND_ON, cloudAccount, compositeName, planName, htmlReportSubject, teamName
 }
@@ -191,7 +172,6 @@ function createConfig(argForConfig) {
         teamName: argForConfig.teamName,
         violations: argForConfig.cloudObjects,
         userSchemes: argForConfig.userSchemes,
-        userSuppression: argForConfig.userSuppression,
         alertList: argForConfig.alertListArray,
         disabled: argForConfig.ruleInputs,
         cloudAccount: argForConfig.cloudAccount
@@ -211,7 +191,6 @@ const CloudCoreoJSRunner = require('cloudcoreo-jsrunner-commons');
 const emails = CloudCoreoJSRunner.createEmails(JSON_INPUT, SETTINGS);
 const suppressionJSON = CloudCoreoJSRunner.createJSONWithSuppress(JSON_INPUT, SETTINGS);
 
-coreoExport('JSONReport', JSON.stringify(suppressionJSON));
 coreoExport('report', JSON.stringify(suppressionJSON['violations']));
 
 callback(emails);
@@ -221,7 +200,7 @@ end
 coreo_uni_util_variables "elb-update-planwide-3" do
   action :set
   variables([
-                {'COMPOSITE::coreo_uni_util_variables.elb-planwide.results' => 'COMPOSITE::coreo_uni_util_jsrunner.elb-tags-to-notifiers-array.JSONReport'},{'COMPOSITE::coreo_aws_rule_runner.advise-elb.report' => 'COMPOSITE::coreo_uni_util_jsrunner.elb-tags-to-notifiers-array.report'},
+                {'COMPOSITE::coreo_aws_rule_runner.advise-elb.report' => 'COMPOSITE::coreo_uni_util_jsrunner.elb-tags-to-notifiers-array.report'},
                 {'GLOBAL::table' => 'COMPOSITE::coreo_uni_util_jsrunner.elb-tags-to-notifiers-array.table'}
             ])
 end
